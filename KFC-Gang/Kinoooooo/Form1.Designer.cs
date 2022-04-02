eb0e95a9cef0528320ff07d5b8f50fc183f718d9
syrovym05 <97514843+syrovym05@users.noreply.github.com>
2022-04-02 13:37:55 +0200

    Add files via upload

---
+++ KFC-Gang/Kinoooooo/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Kinoooooo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(950, 558);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(361, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 80);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "KINOSÁL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(974, 681);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(990, 720);
+            this.MinimumSize = new System.Drawing.Size(990, 720);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KFC-Gang/Kinoooooo/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kinoooooo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int rozmer = 50;
+            panel1.Controls.Clear();
+            int Sloupce = 17;
+            int Radky = 10;
+            int Dvojsedacky = 6;
+            int[,] dvoupole = new int[Sloupce, Radky];
+
+            for (int i = 0; i < Radky; i++)
+            {
+                for (int j = 0; j < Sloupce; j++)
+                {
+                    dvoupole[j, i] = 0;
+                }
+            }
+            for (int i = 1; i <= Radky; i++)
+            {
+                if (i == 1)
+                {
+                    int pomocna = 0;
+                    for (int j = 1; j <= Dvojsedacky; j++)
+                    {
+                        Button tlacitko = new Button();
+                        tlacitko.Text = "" + Convert.ToChar(j + 64) + " " + i.ToString();
+                        tlacitko.ForeColor = Color.FromArgb(60, 198, 50);
+                        tlacitko.Width = rozmer * 2;
+                        tlacitko.Height = rozmer;
+                        tlacitko.Left = rozmer + pomocna;
+                        tlacitko.Top = rozmer;                        
+                        tlacitko.BackgroundImage = Properties.Resources._2sedadlo_volne;
+                        tlacitko.FlatStyle = FlatStyle.Flat;
+                        tlacitko.FlatAppearance.BorderSize = 0;
+                        tlacitko.Click += tlacitko_Click;
+                        panel1.Controls.Add(tlacitko);
+                        pomocna += 150;
+                    }
+                }
+                else
+                {
+                    for (int j = 1; j <= Sloupce; j++)
+                    {
+                        Button tlacitko = new Button();
+                        tlacitko.Text = "" + Convert.ToChar(j + 64) + " " + i.ToString();
+                        tlacitko.ForeColor = Color.FromArgb(60, 198, 50);
+                        tlacitko.Width = rozmer;
+                        tlacitko.Height = rozmer;
+                        tlacitko.Left = rozmer * j;
+                        tlacitko.Top = rozmer * i;
+                        tlacitko.BackgroundImage = Properties.Resources.sedadlo_volne;
+                        tlacitko.FlatStyle = FlatStyle.Flat;
+                        tlacitko.FlatAppearance.BorderSize = 0;
+                        tlacitko.Click += tlacitko_Click;
+                        panel1.Controls.Add(tlacitko);
+                    }
+                }
+            }
+        }
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+           
+        }
+        private void tlacitko_Click(object sender, EventArgs e)
+        {
+            DialogResult vysledek = MessageBox.Show("Chcete zarezervovat sedadlo č. " + (sender as Button).Text + "?", "Objednávka", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(vysledek == DialogResult.Yes)
+            {
+                MessageBox.Show("Sedadlo č. " + (sender as Button).Text + " bylo zarezervováno", "Objednávka", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }       
+    }
+}
+
